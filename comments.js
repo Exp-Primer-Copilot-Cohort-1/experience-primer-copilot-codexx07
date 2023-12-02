5eb54185357d3b4c82820d1640a19c09b946e702
Sparsh Chakraborty <sparsh.riju@gmail.com>
2023-12-02 13:22:53 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,51 @@
+// Create web server
+// Use express framework
+// Use body-parser middleware
+// Use cookie-parser middleware
+// Use express-session middleware
+// Use passport middleware
+// Use connect-flash middleware
+// Use express-handlebars template engine
+// Use connect-mongo middleware
+// Use mongoose middleware
+// Use express-validator middleware
+// Use method-override middleware
+// Use csurf middleware
+// Set view engine
+// Use static middleware
+// Use router
+// Use error handler middleware
+// Listen on port 3000
+// Create server-side rendering for comments
+// Create server-side rendering for pagination
+// Create server-side rendering for comments form
+// Create server-side rendering for comments form error
+// Create server-side rendering for comments form success
+// Create server-side rendering for comments form validation error
+// Create server-side rendering for comments form csrf error
+// Create server-side rendering for comments form csrf token
+// Create server-side rendering for comments form csrf token input
+// Create server-side rendering for comments form csrf token input value
+// Create server-side rendering for comments form csrf token input name
+// Create server-side rendering for comments form csrf token input type
+// Create server-side rendering for comments form csrf token input id
+// Create server-side rendering for comments form csrf token input class
+// Create server-side rendering for comments form csrf token input placeholder
+// Create server-side rendering for comments form csrf token input required
+// Create server-side rendering for comments form csrf token input autofocus
+// Create server-side rendering for comments form csrf token input autocomplete
+// Create server-side rendering for comments form csrf token input spellcheck
+// Create server-side rendering for comments form csrf token input maxlength
+// Create server-side rendering for comments form csrf token input minlength
+// Create server-side rendering for comments form csrf token input pattern
+// Create server-side rendering for comments form csrf token input size
+// Create server-side rendering for comments form csrf token input height
+// Create server-side rendering for comments form csrf token input width
+// Create server-side rendering for comments form csrf token input list
+// Create server-side rendering for comments form csrf token input value
+// Create server-side rendering for comments form csrf token input form
+// Create server-side rendering for comments form csrf token input tabindex
+// Create server-side rendering for comments form csrf token input accesskey
+// Create server-side rendering for comments form csrf token input dir
+// Create server-side rendering for comments form csrf token input formaction
+// Create server-side rendering for
